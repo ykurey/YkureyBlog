9235ac2af7bf7c57967183d5f6472a250f27cfde
ykurey <s1041817@gm.pu.edu.tw>
2019-02-27 15:22:56 +0800

    ~ signin user "show message"

--- app/controllers/signins_controller.rb
+++ app/controllers/signins_controller.rb
@@ -9,14 +9,14 @@ class SigninsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, :notice => "登入成功"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now.alert = "帳號或密碼錯誤"
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "登入成功!"
+    redirect_to root_url, :notice => "登出成功!"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, :notice => "Sign_up"
+      redirect_to root_url, :notice => "註冊成功"
     else
       render "new"
     end
