b1643d5dc8df407b95e37bbd2ee5fd5a05c1a191
ykurey <s1041817@gm.pu.edu.tw>
2019-03-03 16:44:24 +0800

    ~ users 新增帳號bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UsersInformation.create(:email => @user.email, :user_id => @user.id)
       redirect_to root_url, :notice => "註冊成功"
     else
       render "new"
@@ -17,19 +18,15 @@ class UsersController < ApplicationController
 
   def edit
     userName = User.find_by_username(params[:id])
-    puts "---"
     if userName.nil?
-      puts "-1-"
       reUser = User.find_by_id(session[:user_id]).username
       redirect_to edit_user_path(reUser)
     else
       if session[:user_id] == userName.id
-        puts "-2-"
         @userInformation = UsersInformation.find_by_user_id(session[:user_id])
       elsif session[:user_id].nil?
         redirect_to root_path
       else
-        puts "-3-"
         reUser = User.find_by_id(session[:user_id]).userName
         redirect_to edit_user_path(reUser)
       end
@@ -42,7 +39,6 @@ class UsersController < ApplicationController
     if userInformation.update(params.require(:users_information).permit(:name, :email, :birthday, :phone, :address, :about))
       reUser = User.find_by_id(session[:user_id]).username
       redirect_to edit_user_path(reUser)
-      puts "update 成功"
     else
       render :edit
     end
