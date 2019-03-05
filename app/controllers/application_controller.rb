87cb87d59f442a66fc42d2b1efc90f93b9578f93
ykurey <s1041817@gm.pu.edu.tw>
2019-03-05 12:02:41 +0800

    + 登出 + 移除緩存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_no_cache
   helper_method :current_user
-
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 
 end
